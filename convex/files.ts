d90034dcc194411c6047054aaf66fca29ad56d86
admin <harshkurulkar7@gmail.com>
2024-03-02 11:02:28 +0530

    create webhook

--- convex/files.ts
+++ convex/files.ts
@@ -4,25 +4,33 @@ import { mutation, query } from "./_generated/server";
 export const createFile = mutation({
   args: {
     name: v.string(),
+    orgId: v.string(),
   },
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) {
       throw new ConvexError("You must be logged in to create file");
     }
+    
     await ctx.db.insert("files", {
       name: args.name,
+      orgId: args.orgId,
     });
   },
 });
 
 export const getFiles = query({
-  args: {},
+  args: {
+    orgId: v.string(),
+  },
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) {
       return [];
     }
-    return ctx.db.query("files").collect();
+    return ctx.db
+      .query("files")
+      .withIndex("by_orgId", (q) => q.eq("orgId", args.orgId))
+      .collect();
   },
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { SignInButton } from "@clerk/nextjs";
+import { SignInButton, useOrganization } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const { organization } = useOrganization();
   const createFile = useMutation(api.files.createFile);
-  const files = useQuery(api.files.getFiles);
+  const files = useQuery(
+    api.files.getFiles,
+    organization?.id ? {orgId : organization.id} : "skip"
+  );
+  console.log(organization?.id);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignInButton mode="modal"></SignInButton>
       <Button>Hello</Button>
-      <Button onClick={() => createFile({ name: "hello world" })}>
+      <Button
+        onClick={() => {
+          if (!organization) return;
+          createFile({ name: "hello world", orgId: organization.id });
+        }}
+      >
         Click Me
       </Button>
       {files?.map((file) => {
